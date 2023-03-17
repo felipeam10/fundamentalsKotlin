110c9986671a1f1ae3793dc2202b32dcc5884188
felipeam10 <felipeam10@hotmail.com>
2023-03-17 01:14:10 -0300

    atributos e acessos

--- src/main/kotlin/fundamentals/Carro.kt
+++ src/main/kotlin/fundamentals/Carro.kt
@@ -1,4 +1,16 @@
 package fundamentals
 
-class Carro {
+class Carro(var cor: String, val anoFabricacao: Int, val dono: Dono)
+
+data class Dono(var nome: String, var idade: Int)
+
+fun main() {
+    var carro = Carro("Branco", 1996, Dono("Felipe", 1984))
+
+    println(carro.cor)
+    carro.cor = "Preto"
+    println(carro.dono.nome)
+
+    carro.dono.nome = "fulano"
+    println(carro.dono)
 }
