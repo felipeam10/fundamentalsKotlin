a5d4a0307cb0febd612f84b72a4a81bc8d735356
felipeam10 <felipeam10@hotmail.com>
2023-03-27 09:52:08 -0300

    v4.11 - functions, returns and parameters

---
+++ src/main/kotlin/fundamentals/Funcoes.kt
@@ -0,0 +1,31 @@
+package fundamentals
+
+fun main() {
+    println("Hello World")
+
+    println(retornaNome())
+
+    var name = retornaNome1()
+    println(name)
+
+    sayHello("Felipe", 120)
+    sayHello(retornaNome1(), 200)
+    sayHello(age = 112, name1 = "Matusalen")
+
+    sayHello1("IdadeFixa")
+}
+
+fun retornaNome(): String{
+    return "Felipe"
+}
+
+fun retornaNome1(): String{
+    return "Felipe1"
+}
+
+fun sayHello(name1: String, age: Int) {
+    println("Hello ${name1}, congrats for yous ${age} years old")
+}
+fun sayHello1(name2: String, age: Int = 20) {
+    println("Hello ${name2}, congrats for yous ${age} years old")
+}
