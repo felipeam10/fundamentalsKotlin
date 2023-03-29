d29432398bc8bb4721213e3c9fca06fd2fce5002
felipeam10 <felipeam10@hotmail.com>
2023-03-29 09:51:44 -0300

    v4.12 - variables

---
+++ src/main/kotlin/fundamentals/Variables.kt
@@ -0,0 +1,25 @@
+package fundamentals
+
+fun main() {
+
+    var nome = "Felipe"
+    val nomeVal = "Felipe"
+
+    nome = "Felipao"
+    //nomeVal = "Felipao2"
+
+    val idade = 200
+    println(idade)
+
+    val teste: String
+    teste = "Teste"
+
+}
+
+class Variables {
+    lateinit var teste: String
+
+    fun iniciaVariaveis() {
+        teste = "Teste"
+    }
+}
